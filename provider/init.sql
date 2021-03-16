a99690ca1293df3c906dcc436ecde29972036e57
Marco Lehmann <m99@posteo.de>
2021-03-16 14:57:31 +0100

    Make it working :)

--- provider/init.sql
+++ provider/init.sql
@@ -1,37 +1,56 @@
--- create replicator user
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'my_replicator_password';
+-- create database
+CREATE DATABASE pg_logical_replication;
+\c pg_logical_replication;
 
--- dummy data
-CREATE TABLE sensor_log (
-  id           SERIAL PRIMARY KEY NOT NULL,
-  location     VARCHAR NOT NULL,
-  reading      BIGINT NOT NULL,
-  reading_date TIMESTAMP NOT NULL
-);
+-- create table
+CREATE TABLE hashes (id SERIAL, value CHAR(33), PRIMARY KEY(value));
 
-INSERT INTO sensor_log (location, reading, reading_date)
-SELECT s.id % 1000, s.id % 100, CURRENT_DATE - (s.id || 's')::INTERVAL
-  FROM generate_series(1, 1000000) s(id);
+-- create data
+INSERT INTO hashes (SELECT generate_series(1, 1000), md5(random()::TEXT));
 
--- create extension
-CREATE EXTENSION pglogical;
+-- create role
+CREATE ROLE replicate WITH LOGIN PASSWORD 'qwertz' REPLICATION;
 
--- create node
-SELECT pglogical.create_node(
-  node_name := 'provider',
-  dsn := 'host=127.0.0.1 port=5432 dbname=logging user=replicator password=my_replicator_password'
-);
+-- create publication
+CREATE PUBLICATION pub_hashes FOR TABLE hashes;
 
--- create replication set
-SELECT pglogical.create_replication_set(
-  set_name := 'logging',
-  replicate_insert := TRUE,
-  replicate_update := FALSE,
-  replicate_delete := FALSE,
-  replicate_truncate := FALSE
-);
-SELECT pglogical.replication_set_add_table(
-  set_name := 'logging',
-  relation := 'sensor_log',
-  synchronize_data := TRUE
-);
+-- grant rights
+GRANT SELECT ON hashes TO replicate;
+
+-- -- create replicator user
+-- CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'my_replicator_password';
+
+-- -- dummy data
+-- CREATE TABLE sensor_log (
+--   id           SERIAL PRIMARY KEY NOT NULL,
+--   location     VARCHAR NOT NULL,
+--   reading      BIGINT NOT NULL,
+--   reading_date TIMESTAMP NOT NULL
+-- );
+
+-- INSERT INTO sensor_log (location, reading, reading_date)
+-- SELECT s.id % 1000, s.id % 100, CURRENT_DATE - (s.id || 's')::INTERVAL
+--   FROM generate_series(1, 1000000) s(id);
+
+-- -- create extension
+-- CREATE EXTENSION pglogical;
+
+-- -- create node
+-- SELECT pglogical.create_node(
+--   node_name := 'provider',
+--   dsn := 'host=127.0.0.1 port=5432 dbname=logging user=replicator password=my_replicator_password'
+-- );
+
+-- -- create replication set
+-- SELECT pglogical.create_replication_set(
+--   set_name := 'logging',
+--   replicate_insert := TRUE,
+--   replicate_update := FALSE,
+--   replicate_delete := FALSE,
+--   replicate_truncate := FALSE
+-- );
+-- SELECT pglogical.replication_set_add_table(
+--   set_name := 'logging',
+--   relation := 'sensor_log',
+--   synchronize_data := TRUE
+-- );
--- subscriber/init.sql
+++ subscriber/init.sql
@@ -1,27 +1,37 @@
--- dummy data
-CREATE TABLE sensor_log (
-  id           INT PRIMARY KEY NOT NULL,
-  location     VARCHAR NOT NULL,
-  reading      BIGINT NOT NULL,
-  reading_date TIMESTAMP NOT NULL
-);
+-- create database
+CREATE DATABASE pg_logical_replication_results;
+\c pg_logical_replication_results;
 
--- create extension
-CREATE EXTENSION pglogical;
-
--- create node
-SELECT pglogical.create_node(
-  node_name := 'subscriber',
-  dsn := 'host=127.0.0.1 port=5999 dbname=logging user=replicator password=my_replicator_password'
-);
+-- create table
+CREATE TABLE hashes (id SERIAL, value CHAR(33), PRIMARY KEY(value));
 
 -- create subscription
-SELECT pglogical.create_subscription(
-  subscription_name := 'subscription',
-  replication_sets := array['logging'],
-  provider_dsn := 'host=127.0.0.1 port=5432 dbname=logging user=replicator password=my_replicator_password'
-);
+CREATE SUBSCRIPTION sub_hashes CONNECTION 'host=pgprovider dbname=pg_logical_replication user=replicate password=qwertz' PUBLICATION pub_hashes;
 
--- test replication
-SELECT pg_sleep(5);
-SELECT COUNT(*) FROM sensor_log;
+-- -- dummy data
+-- CREATE TABLE sensor_log (
+--   id           INT PRIMARY KEY NOT NULL,
+--   location     VARCHAR NOT NULL,
+--   reading      BIGINT NOT NULL,
+--   reading_date TIMESTAMP NOT NULL
+-- );
+
+-- -- create extension
+-- CREATE EXTENSION pglogical;
+
+-- -- create node
+-- SELECT pglogical.create_node(
+--   node_name := 'subscriber',
+--   dsn := 'host=127.0.0.1 port=5999 dbname=logging user=replicator password=my_replicator_password'
+-- );
+
+-- -- create subscription
+-- SELECT pglogical.create_subscription(
+--   subscription_name := 'subscription',
+--   replication_sets := array['logging'],
+--   provider_dsn := 'host=127.0.0.1 port=5432 dbname=logging user=replicator password=my_replicator_password'
+-- );
+
+-- -- test replication
+-- SELECT pg_sleep(5);
+-- SELECT COUNT(*) FROM sensor_log;
